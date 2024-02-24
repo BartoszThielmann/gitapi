b40ea7ec32ee47b78cfd5bfd38e7b2257999c15d
BartoszThielmann <bartoszthielmann@gmail.com>
2024-02-24 13:25:54 +0100

    Refactor to imperative code
    *Change reactive WebClient to imperative RestTemplate
    
    *Configured RestTemplate as Bean
    
    This commit is a downgrade compared to master, since Repository branches
    are no longer fetched. It will be added in subsequent commits.

---
+++ src/main/java/com/bartoszthielmann/gitapi/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.bartoszthielmann.gitapi.config;
+
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(6);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(120)) // timeout to get connection from pool
+                .setConnectTimeout(Timeout.ofSeconds(120)) // standard connection timeout
+                .build();
+
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig).build();
+
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+}
--- src/main/java/com/bartoszthielmann/gitapi/controller/RepositoryController.java
+++ src/main/java/com/bartoszthielmann/gitapi/controller/RepositoryController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
@@ -20,8 +21,8 @@ public class RepositoryController {
     }
 
     @GetMapping(value = "/repos/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<RepositoryDto> findUserReposByUsername(@PathVariable String username) {
-        Flux<RepositoryDto> repos = repositoryService.getUserRepos(username);
+    public List<RepositoryDto> findUserReposByUsername(@PathVariable String username) {
+        List<RepositoryDto> repos = repositoryService.getUserRepos(username);
         return repos;
     }
 }
