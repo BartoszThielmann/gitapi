4366e84b034503da094454e22ebbf34640ddf710
BartoszThielmann <bartoszthielmann@gmail.com>
2024-02-25 10:24:02 +0100

    Handle 404 error when fetching user's repos
    As per business requirements return json containing status code and
    error message in case when user doesn't exist.

--- src/main/java/com/bartoszthielmann/gitapi/client/GitHubWebClient.java
+++ src/main/java/com/bartoszthielmann/gitapi/client/GitHubWebClient.java
@@ -2,10 +2,13 @@ package com.bartoszthielmann.gitapi.client;
 
 import com.bartoszthielmann.gitapi.entity.Branch;
 import com.bartoszthielmann.gitapi.entity.Repository;
+import com.bartoszthielmann.gitapi.exception.UserNotFoundException;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -22,12 +25,17 @@ public class GitHubWebClient {
 
     public List<Repository> getUserRepos(String username) {
         String url = BASE_URL + "/users/" + username + "/repos?per_page=100";
-        ResponseEntity<List<Repository>> response =
-                restTemplate.exchange(url,
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Repository>>() {
-                        });
-        List<Repository> repositories = response.getBody();
-        return repositories;
+        ResponseEntity<List<Repository>> response;
+        try {
+        response = restTemplate.exchange(url,
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Repository>>() {});
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new UserNotFoundException();
+            }
+            throw e;
+        }
+        return response.getBody();
     }
 
     public List<Branch> getRepoBranches(String owner, String repo) {
