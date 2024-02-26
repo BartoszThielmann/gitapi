a45aa890fffd5973f7ee90757173d71e9a5607a8
BartoszThielmann <bartoszthielmann@gmail.com>
2024-02-26 18:04:04 +0100

    Handle paginated responses from GitHub
    When GitHub sends paginated responses, Link header is included, where
    url to "next" page can be found. When getting data for branches or
    repositories, try to get url to the next page, and fetch data until
    there is no next page (last page is reached).
    
    Current implementation contains (some amount of) repetetive code,
    probably also Client contains logic which should be in Service layer.
    This is going to be subject for further optimizations/refactoring.

--- src/main/java/com/bartoszthielmann/gitapi/client/GitHubWebClient.java
+++ src/main/java/com/bartoszthielmann/gitapi/client/GitHubWebClient.java
@@ -4,6 +4,7 @@ import com.bartoszthielmann.gitapi.entity.Branch;
 import com.bartoszthielmann.gitapi.entity.Repository;
 import com.bartoszthielmann.gitapi.exception.UserNotFoundException;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,39 +12,82 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class GitHubWebClient {
 
     private final RestTemplate restTemplate;
     private static final String BASE_URL = "https://api.github.com";
+    private static final Pattern NEXT_PAGE_URL_PATTERN = Pattern.compile("<([^>]+)>; rel=\"next\"");
 
     public GitHubWebClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public List<Repository> getUserRepos(String username) {
+        ResponseEntity<List<Repository>> currPageResponse;
+        List<Repository> allRepositories = new ArrayList<>();
         String url = BASE_URL + "/users/" + username + "/repos?per_page=100";
-        ResponseEntity<List<Repository>> response;
-        try {
-        response = restTemplate.exchange(url,
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<Repository>>() {});
-        } catch (HttpStatusCodeException e) {
-            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new UserNotFoundException();
+
+        while (url != null) {
+            try {
+                currPageResponse = restTemplate.exchange(url,
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Repository>>() {
+                        });
+            } catch (HttpStatusCodeException e) {
+                if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                    throw new UserNotFoundException();
+                }
+                throw e;
             }
-            throw e;
+
+            List<Repository> currPageRepositories = currPageResponse.getBody();
+            if (currPageRepositories != null) {
+                allRepositories.addAll(currPageRepositories);
+            }
+
+            List<String> linkHeader = currPageResponse.getHeaders().get(HttpHeaders.LINK);
+            url = getNextPageUrl(linkHeader);
         }
-        return response.getBody();
+        return allRepositories;
     }
 
     public List<Branch> getRepoBranches(String owner, String repo) {
+        ResponseEntity<List<Branch>> currPageResponse;
+        List<Branch> allBranches = new ArrayList<>();
         String url = BASE_URL + "/repos/" + owner + "/" + repo + "/branches?per_page=100";
-        ResponseEntity<List<Branch>> response =
+
+        while (url != null) {
+            currPageResponse =
                 restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Branch>>() {
                 });
-        List<Branch> branches = response.getBody();
-        return branches;
+
+            List<Branch> currPageRepositories = currPageResponse.getBody();
+            if (currPageRepositories != null) {
+                allBranches.addAll(currPageRepositories);
+            }
+
+            List<String> linkHeader = currPageResponse.getHeaders().get(HttpHeaders.LINK);
+            url = getNextPageUrl(linkHeader);
+        }
+        return allBranches;
+    }
+
+    private String getNextPageUrl(List<String> linkHeader) {
+        if (linkHeader == null) {
+            return null;
+        }
+        return linkHeader.stream()
+                .filter(link -> link.contains("rel=\"next\""))
+                .map(link -> {
+                    Matcher matcher = NEXT_PAGE_URL_PATTERN.matcher(link);
+                    return matcher.find() ? matcher.group(1) : null;
+                })
+                .findFirst()
+                .orElse(null);
     }
 }
